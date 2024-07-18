5c1c129dcd68e439d34acfb2650330885be07f24
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-18 14:35:59 +0530

    Add files via upload

--- address of elements.cpp
+++ address of elements.cpp
@@ -1,3 +1,21 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+	cout<<"hello India"<<endl;
+	cout<<"jai ho";
+	return 0;	
+}*/
+#include<iostream>
+using namespace std;
+int main(){
+	int a , b ,c;
+	  a=15;
+	  b=16;
+	  c=51;
+	  cout<<"a*b"
+
+}
+/*
 #include<iostream>
 using namespace std;
 int main(){
@@ -6,4 +24,4 @@ int main(){
 	int b;
 	cout<<"The size of int is :"<<sizeof(b)<<endl<<"The size of long long int is: "<<sizeof(a);
 	return 0;
-}
+}*/
