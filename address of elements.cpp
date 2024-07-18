d8e1b66829fcfa5f27c63a361429d431ca393a65
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-18 14:37:41 +0530

    Update address of elements.cpp

--- address of elements.cpp
+++ address of elements.cpp
@@ -1,21 +1,4 @@
-/*#include<iostream>
-using namespace std;
-int main(){
-	cout<<"hello India"<<endl;
-	cout<<"jai ho";
-	return 0;	
-}*/
-#include<iostream>
-using namespace std;
-int main(){
-	int a , b ,c;
-	  a=15;
-	  b=16;
-	  c=51;
-	  cout<<"a*b"
-
-}
-/*
+ 
 #include<iostream>
 using namespace std;
 int main(){
@@ -24,4 +7,4 @@ int main(){
 	int b;
 	cout<<"The size of int is :"<<sizeof(b)<<endl<<"The size of long long int is: "<<sizeof(a);
 	return 0;
-}*/
+}
